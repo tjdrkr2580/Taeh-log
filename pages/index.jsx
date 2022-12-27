0538306b68267510c99a336c121dfdbd4f38774f
KimTaeHyun <tjdrkr2580@naver.com>
2022-12-27 13:19:14 +0900

    add postlist data, desc ,category

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,39 @@
 import IndexStyle from "../styles/index.module.scss";
+import { serialize } from "next-mdx-remote/serialize";
 import path from "path";
 import fs from "fs";
 import matter from "gray-matter";
 
 function Home({ posts }) {
-  return <div className={IndexStyle.taeh}>메인</div>;
+  return (
+    <div className={IndexStyle.taeh}>
+      <ul className={IndexStyle.postListWrapper}>
+        {posts.map((post, index) => (
+          <li className={IndexStyle.postlist} key={index}>
+            <h1>{post.data.title}</h1>
+            <p>{post.data.desc}</p>
+            <span>{post.data.date}</span>
+            <div>{post.data.category}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export const getStaticProps = () => {
-  const POST_PATH = path.join(process.cwd(), "public/posts");
-  const postFilePaths = fs.readdirSync(POST_PATH).map((post) => {
-    const source = fs.readFileSync(path.join(POST_PATH, post));
-    const { content, data } = matter(source);
-    console.log(content, data);
+export const getStaticProps = async () => {
+  const post_files = fs.readdirSync(path.join(process.cwd(), "public/posts"));
+  const posts = post_files.map((filePath) => {
+    const post_read = fs.readFileSync(
+      path.join(process.cwd(), `public/posts/${filePath}`)
+    );
+    const { data } = matter(post_read);
+    return { data };
   });
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
 
